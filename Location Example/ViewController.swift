66bee4ee7877d7b886bc54c16798e28a9fd639b4
Sumeet Sharma <sumeetsharma@ucla.edu>
2014-09-15 16:45:05 -0700

    changes to handle xcode 6 beta 6

--- Location Example/ViewController.swift
+++ Location Example/ViewController.swift
@@ -13,59 +13,63 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 
 	let locationManager = CLLocationManager()
     
-    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: AnyObject!) {
-        //(AnyObject[]!, NSError!) -> Void
-        //func reverseGeocodeLocation(location: CLLocation!, completionHandler: CLGeocodeCompletionHandler!)
-        func foo (placemarks: AnyObject[]!, error: NSError!) -> Void {
-            if error {
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: AnyObject!)
+    {
+        func foo (placemarks: [AnyObject]!, error: NSError!) -> Void
+        {
+            if error != nil
+            {
                 println("Reverse geocode failed: " + error.localizedDescription)
                 return
             }
             println("got some location data")
-            if placemarks.count > 0 {
+            if placemarks.count > 0
+            {
                 let pm = placemarks[0] as CLPlacemark
                 self.displayLocationInfo(pm)
-            } else {
+            } else
+            {
                 println("Problem with the data received from geocoder")
             }
         }
         CLGeocoder().reverseGeocodeLocation(manager.location, foo)
     }
     
-    func displayLocationInfo(placemark: CLPlacemark) {
+    func displayLocationInfo(placemark: CLPlacemark)
+    {
         println("displaying location info")
-        if placemark != nil {
-            //locationManager.stopUpdatingLocation()
-            placemark.location.co
-            println(placemark.locality ? placemark.locality : "")
-            println(placemark.postalCode ? placemark.postalCode : "")
-            println(placemark.administrativeArea ? placemark.administrativeArea : "")
-            println(placemark.country ? placemark.country : "")
-        }
+        //locationManager.stopUpdatingLocation()
+        //placemark.location.co
+        println(placemark.locality != nil ? placemark.locality : "")
+        println(placemark.postalCode != nil ? placemark.postalCode : "")
+        println(placemark.administrativeArea != nil ? placemark.administrativeArea : "")
+        println(placemark.country != nil ? placemark.country : "")
     }
     
-    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
+    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!)
+    {
         println("error" + error.localizedDescription)
     }
                             
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         locationManager.requestWhenInUseAuthorization()
     }
     
     // UI button callback
-    @IBAction func findMyLocation(sender: AnyObject) {
+    @IBAction func findMyLocation(sender: AnyObject)
+    {
     	println("getting updated location...")
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.startUpdatingLocation()
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
